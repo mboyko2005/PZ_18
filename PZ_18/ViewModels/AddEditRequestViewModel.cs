2cb6234da44f202af6e6f505723336d0f478de45
Максим Бойко <mboyko2005@gmail.com>
2024-12-07 09:50:45 +0500

    Поменял редактированиие окна элементы и добавил новые

--- PZ_18/ViewModels/AddEditRequestViewModel.cs
+++ PZ_18/ViewModels/AddEditRequestViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 using PZ_18.Data;
@@ -6,57 +7,85 @@ using PZ_18.Models;
 
 namespace PZ_18.ViewModels
 {
-	/// <summary>
-	/// ViewModel для окна добавления/редактирования заявки.
-	/// </summary>
-	public class AddEditRequestViewModel : INotifyPropertyChanged
-	{
-		private Request _currentRequest;
+    /// <summary>
+    /// ViewModel для окна добавления/редактирования заявки.
+    /// </summary>
+    public class AddEditRequestViewModel : INotifyPropertyChanged
+    {
+        private Request _currentRequest;
 
-		public Request CurrentRequest
-		{
-			get => _currentRequest;
-			set { _currentRequest = value; OnPropertyChanged(nameof(CurrentRequest)); }
-		}
+        /// <summary>
+        /// Текущая заявка.
+        /// </summary>
+        public Request CurrentRequest
+        {
+            get => _currentRequest;
+            set
+            {
+                _currentRequest = value;
+                OnPropertyChanged(nameof(CurrentRequest));
+            }
+        }
 
-		public ICommand SaveCommand { get; }
+        /// <summary>
+        /// Список возможных статусов заявки.
+        /// </summary>
+        public ObservableCollection<string> StatusOptions { get; } = new ObservableCollection<string>
+        {
+            "Новая заявка",
+            "В процессе ремонта",
+            "Ожидание запчастей",
+            "Готова к выдаче"
+        };
 
-		public AddEditRequestViewModel(Request request = null)
-		{
-			if (request == null)
-			{
-				CurrentRequest = new Request
-				{
-					StartDate = DateTime.Now,
-					RequestStatus = "Новая заявка"
-				};
-			}
-			else
-			{
-				CurrentRequest = request;
-			}
+        /// <summary>
+        /// Команда для сохранения заявки.
+        /// </summary>
+        public ICommand SaveCommand { get; }
 
-			SaveCommand = new RelayCommand(SaveRequest);
-		}
+        /// <summary>
+        /// Конструктор ViewModel.
+        /// </summary>
+        /// <param name="request">Заявка, которую редактируем или добавляем.</param>
+        public AddEditRequestViewModel(Request request = null)
+        {
+            if (request == null)
+            {
+                CurrentRequest = new Request
+                {
+                    StartDate = DateTime.Now,
+                    RequestStatus = "Новая заявка"
+                };
+            }
+            else
+            {
+                CurrentRequest = request;
+            }
 
-		private void SaveRequest(object obj)
-		{
-			using (var context = new CoreContext())
-			{
-				if (CurrentRequest.RequestID == 0)
-				{
-					context.Requests.Add(CurrentRequest);
-				}
-				else
-				{
-					context.Requests.Update(CurrentRequest);
-				}
-				context.SaveChanges();
-			}
-		}
+            SaveCommand = new RelayCommand(SaveRequest);
+        }
 
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected void OnPropertyChanged(string propName) =>
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-	}
+        /// <summary>
+        /// Логика сохранения заявки.
+        /// </summary>
+        private void SaveRequest(object obj)
+        {
+            using (var context = new CoreContext())
+            {
+                if (CurrentRequest.RequestID == 0)
+                {
+                    context.Requests.Add(CurrentRequest);
+                }
+                else
+                {
+                    context.Requests.Update(CurrentRequest);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+    }
 }
