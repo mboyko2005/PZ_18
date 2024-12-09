d13d6eb904fa5323ca31593b0cfbc22bb8b3707a
Максим Бойко <mboyko2005@gmail.com>
2024-12-09 20:01:36 +0500

    добавил и сделал пз 19

--- PZ_18/Models/Request.cs
+++ PZ_18/Models/Request.cs
@@ -8,124 +8,127 @@ using PZ_18.Models.Interfaces;
 
 namespace PZ_18.Models
 {
-    /// <summary>
-    /// Заявка на ремонт.
-    /// </summary>
-    public class Request : INotifyPropertyChanged, IRequest
-    {
-        private int _requestId;
-        private DateTime _startDate;
-        private int _techTypeId;
-        private string _homeTechModel;
-        private string _problemDescription;
-        private string _requestStatus;
-        private DateTime? _completionDate;
-        private int? _masterId;
-        private int? _clientId;
-        private string _clientFIO;
-        private string _clientPhone;
+	/// <summary>
+	/// Заявка на ремонт.
+	/// </summary>
+	public class Request : INotifyPropertyChanged, IRequest
+	{
+		private int _requestId;
+		private DateTime _startDate;
+		private int _techTypeId;
+		private string _homeTechModel;
+		private string _problemDescription;
+		private string _requestStatus;
+		private DateTime? _completionDate;
+		private int? _masterId;
+		private int? _clientId;
+		private string _clientFIO;
+		private string _clientPhone;
 
-        [Key]
-        public int RequestID
-        {
-            get => _requestId;
-            private set { _requestId = value; OnPropertyChanged(); }
-        }
+		[Key]
+		public int RequestID
+		{
+			get => _requestId;
+			private set { _requestId = value; OnPropertyChanged(); }
+		}
 
-        [Required]
-        public DateTime StartDate
-        {
-            get => _startDate;
-            set { _startDate = value; OnPropertyChanged(); }
-        }
+		[Required]
+		public DateTime StartDate
+		{
+			get => _startDate;
+			set { _startDate = value; OnPropertyChanged(); }
+		}
 
-        [Required]
-        public int TechTypeID
-        {
-            get => _techTypeId;
-            set { _techTypeId = value; OnPropertyChanged(); }
-        }
+		[Required]
+		public int TechTypeID
+		{
+			get => _techTypeId;
+			set { _techTypeId = value; OnPropertyChanged(); }
+		}
 
-        [ForeignKey("TechTypeID")]
-        [JsonIgnore]
-        public virtual HomeTechType HomeTechType { get; private set; }
+		[ForeignKey("TechTypeID")]
+		[JsonIgnore]
+		public virtual HomeTechType HomeTechType { get; private set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string HomeTechModel
-        {
-            get => _homeTechModel;
-            set { _homeTechModel = value; OnPropertyChanged(); }
-        }
+		// Вычисляемое свойство для имени типа техники
+		public string TechTypeName => HomeTechType?.TechTypeName ?? "Не указан";
 
-        [Required]
-        public string ProblemDescription
-        {
-            get => _problemDescription;
-            set { _problemDescription = value; OnPropertyChanged(); }
-        }
+		[Required]
+		[MaxLength(255)]
+		public string HomeTechModel
+		{
+			get => _homeTechModel;
+			set { _homeTechModel = value; OnPropertyChanged(); }
+		}
 
-        [Required]
-        [MaxLength(50)]
-        public string RequestStatus
-        {
-            get => _requestStatus;
-            set { _requestStatus = value; OnPropertyChanged(); }
-        }
+		[Required]
+		public string ProblemDescription
+		{
+			get => _problemDescription;
+			set { _problemDescription = value; OnPropertyChanged(); }
+		}
 
-        public DateTime? CompletionDate
-        {
-            get => _completionDate;
-            set { _completionDate = value; OnPropertyChanged(); }
-        }
+		[Required]
+		[MaxLength(50)]
+		public string RequestStatus
+		{
+			get => _requestStatus;
+			set { _requestStatus = value; OnPropertyChanged(); }
+		}
 
-        public int? MasterID
-        {
-            get => _masterId;
-            set { _masterId = value; OnPropertyChanged(); }
-        }
+		public DateTime? CompletionDate
+		{
+			get => _completionDate;
+			set { _completionDate = value; OnPropertyChanged(); }
+		}
 
-        [JsonIgnore]
-        public virtual User Master { get; private set; }
+		public int? MasterID
+		{
+			get => _masterId;
+			set { _masterId = value; OnPropertyChanged(); }
+		}
 
-        public int? ClientID
-        {
-            get => _clientId;
-            set { _clientId = value; OnPropertyChanged(); }
-        }
+		[JsonIgnore]
+		public virtual User Master { get; private set; }
 
-        [JsonIgnore]
-        public virtual User Client { get; private set; }
+		public int? ClientID
+		{
+			get => _clientId;
+			set { _clientId = value; OnPropertyChanged(); }
+		}
 
-        [Required]
-        [MaxLength(255)]
-        public string ClientFIO
-        {
-            get => _clientFIO;
-            set { _clientFIO = value; OnPropertyChanged(); }
-        }
+		[JsonIgnore]
+		public virtual User Client { get; private set; }
 
-        [Required]
-        [MaxLength(15)]
-        public string ClientPhone
-        {
-            get => _clientPhone;
-            set { _clientPhone = value; OnPropertyChanged(); }
-        }
+		[Required]
+		[MaxLength(255)]
+		public string ClientFIO
+		{
+			get => _clientFIO;
+			set { _clientFIO = value; OnPropertyChanged(); }
+		}
 
-        public void UpdateStatus(string newStatus)
-        {
-            RequestStatus = newStatus;
-            if (newStatus == "Готова к выдаче")
-            {
-                CompletionDate = DateTime.Now;
-            }
-        }
+		[Required]
+		[MaxLength(15)]
+		public string ClientPhone
+		{
+			get => _clientPhone;
+			set { _clientPhone = value; OnPropertyChanged(); }
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+		public void UpdateStatus(string newStatus)
+		{
+			RequestStatus = newStatus;
+			if (newStatus == "Готова к выдаче")
+			{
+				CompletionDate = DateTime.Now;
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
 }
--- PZ_18/ViewModels/MainViewModel.cs
+++ PZ_18/ViewModels/MainViewModel.cs
@@ -7,48 +7,50 @@ using System.Windows.Input;
 
 namespace PZ_18.ViewModels
 {
-    /// <summary>
-    /// Главная ViewModel приложения.
-    /// Отвечает за загрузку списков пользователей и заявок, а также поиск заявок.
-    /// </summary>
-    public class MainViewModel
-    {
-        public ObservableCollection<User> Users { get; set; }
-        public ObservableCollection<Request> Requests { get; set; }
+	/// <summary>
+	/// Главная ViewModel приложения.
+	/// Отвечает за загрузку списков пользователей и заявок, а также поиск заявок.
+	/// </summary>
+	public class MainViewModel
+	{
+		public ObservableCollection<User> Users { get; set; }
+		public ObservableCollection<Request> Requests { get; set; }
 
-        public ICommand SearchRequestsCommand { get; }
+		public ICommand SearchRequestsCommand { get; }
 
-        public string SearchText { get; set; }
+		public string SearchText { get; set; }
 
-        public MainViewModel()
-        {
-            // Загрузка данных из контекста (пользователи и заявки).
-            using (var context = new CoreContext())
-            {
-                Users = new ObservableCollection<User>(context.Users.Include(u => u.UserType).ToList());
-                Requests = new ObservableCollection<Request>(context.Requests.ToList());
-            }
+		public MainViewModel()
+		{
+			// Загрузка данных из контекста (пользователи и заявки).
+			using (var context = new CoreContext())
+			{
+				Users = new ObservableCollection<User>(context.Users.Include(u => u.UserType).ToList());
+				Requests = new ObservableCollection<Request>(
+					context.Requests.Include(r => r.HomeTechType).ToList()
+				);
+			}
 
-            SearchRequestsCommand = new RelayCommand(SearchRequests);
-        }
+			SearchRequestsCommand = new RelayCommand(SearchRequests);
+		}
 
-        /// <summary>
-        /// Логика поиска заявок по ФИО клиента.
-        /// </summary>
-        public void SearchRequests(object parameter)
-        {
-            using (var context = new CoreContext())
-            {
-                var query = context.Requests.AsQueryable();
-                if (!string.IsNullOrEmpty(SearchText))
-                {
-                    query = query.Where(r => r.ClientFIO.Contains(SearchText));
-                }
-                var list = query.ToList();
-                Requests.Clear();
-                foreach (var r in list)
-                    Requests.Add(r);
-            }
-        }
-    }
+		/// <summary>
+		/// Логика поиска заявок по ФИО клиента.
+		/// </summary>
+		public void SearchRequests(object parameter)
+		{
+			using (var context = new CoreContext())
+			{
+				var query = context.Requests.Include(r => r.HomeTechType).AsQueryable();
+				if (!string.IsNullOrEmpty(SearchText))
+				{
+					query = query.Where(r => r.ClientFIO.Contains(SearchText));
+				}
+				var list = query.ToList();
+				Requests.Clear();
+				foreach (var r in list)
+					Requests.Add(r);
+			}
+		}
+	}
 }
